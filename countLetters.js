100b4f9368c91b7fdd1c39e6bf9f5a2e72e8f558
LeonXZhou <leon.zhou@queensu.ca>
2021-10-27 22:26:52 +0000

    added tet cases for counter letters.js

--- countLetters.js
+++ countLetters.js
@@ -1,3 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅   Assertion Passed: ${actual} (${typeof(actual)}) === ${expected} (${typeof(expected)})`);
+  } else {
+    console.log(`⚠️   Assertion Failed: ${actual} (${typeof(actual)}) !== ${expected} (${typeof(expected)})`);
+  }
+};
+
 let countLetters = function(string) {
   const results = {};
   for (const s of string) {
@@ -6,8 +15,18 @@ let countLetters = function(string) {
     } else if (s !== ' ') {
       results[s] = 1;
     }
-
   }
   return results;
 };
+
+assertEqual(countLetters("lighthouse in the house")['l'],1);
+assertEqual(countLetters("lighthouse in the house")['i'],2);
+assertEqual(countLetters("lighthouse in the house")['g'],1);
+assertEqual(countLetters("lighthouse in the house")['h'],4);
+assertEqual(countLetters("lighthouse in the house")['t'],2);
+assertEqual(countLetters("lighthouse in the house")['o'],2);
+assertEqual(countLetters("lighthouse in the house")['u'],2);
+assertEqual(countLetters("lighthouse in the house")['s'],2); 
+assertEqual(countLetters("lighthouse in the house")['e'],3);  
+assertEqual(countLetters("lighthouse in the house")['n'],1); 
 console.log(countLetters("lighthouse in the house"));
