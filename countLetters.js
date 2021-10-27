9a08be86d8f8e59d8ae9824dd38892ce3b4395b5
LeonXZhou <leon.zhou@queensu.ca>
2021-10-27 22:18:38 +0000

    linted countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -1,17 +1,13 @@
-let countLetters = function(string)
-{
+let countLetters = function(string) {
   const results = {};
-  for (s of string) {
-    if (results[s] && s !== ' ')
-    {
+  for (const s of string) {
+    if (results[s] && s !== ' ') {
       results[s]++;
-    }
-    else if (s !== ' ')
-    {
+    } else if (s !== ' ') {
       results[s] = 1;
     }
 
   }
-  return results
-}
-console.log(countLetters("lighthouse in the house"))
+  return results;
+};
+console.log(countLetters("lighthouse in the house"));
