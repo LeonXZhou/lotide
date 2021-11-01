e4ae576f07a8eeb8165d9e36b2ad4ebd4e3fa518
LeonXZhou <leon.zhou@queensu.ca>
2021-11-01 23:09:39 +0000

    add mocha and chai tests for tail and middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,17 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArrayEqual = require('../assertArrayEqual');
 
-assertArrayEqual(middle([1,2,3,4]),[2,3]);
-assertArrayEqual(middle([1,2,3]),[2]);
-assertArrayEqual(middle([1,2]),[]);
-assertArrayEqual(middle([1]),[]);
+describe('#Middle should return the middle elements of an array',()=>{
+  it('[1,2,3,4]) => [2,3]',() => {
+    assert.deepEqual(middle([1,2,3,4]),[2,3]);
+  });
+  it('[1,2,3] => [2]',() => {
+    assert.deepEqual(middle([1,2,3]),[2]);
+  });
+  it('[1,2] => []',() => {
+    assert.deepEqual(middle([1,2]),[]);
+  });
+  it('[1] => []',() => {
+    assert.deepEqual(middle([1]),[]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,26 @@
-const assertArrayEqual = require('../assertArrayEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-console.log(tail([1]));
-console.log(tail([]));
-assertArrayEqual(tail([1,1,3]),[1,3]);
-assertArrayEqual(tail([1]),[]);
+// console.log(tail([1]));
+// console.log(tail([]));
+// assertArrayEqual(tail([1,1,3]),[1,3]);
+// assertArrayEqual(tail([1]),[]);
 
 //testing unmodified original array
-let originalArray = [1,2,3,4,5];
-tail(originalArray);
-assertArrayEqual(originalArray,[1,2,3,4,5]);
+// let originalArray = [1,2,3,4,5];
+// tail(originalArray);
+// assertArrayEqual(originalArray,[1,2,3,4,5]);
+
+describe('#tail should return all elements of an array except the first one', ()=>{
+  it('[1] => []',()=>{
+    assert.deepEqual(tail([1,1,3]),[1,3])
+  });
+  it('[1,2,3,4,5] => [2,3,4,5]',()=>{
+    assert.deepEqual(tail([1,1,3]),[1,3])
+  });
+  it('should not modify original array',()=>{
+    let originalArray = [1,2,3,4,5];
+    tail(originalArray);
+    assert.deepEqual(originalArray,[1,2,3,4,5]);
+  });
+});
